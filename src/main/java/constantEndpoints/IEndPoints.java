d203f2224316ad4a668b6e2886a0555dc0166baf
R Suthej <R Suthej@FFE-R-Suthej>
2026-03-11 11:31:01 +0530

    Final Commit

--- src/main/java/constantEndpoints/IEndPoints.java
+++ src/main/java/constantEndpoints/IEndPoints.java
@@ -2,8 +2,6 @@ package constantEndpoints;
 
 public interface IEndPoints {
 
-	  String BASE_URI="https://www.shoppersstack.com/shopping";
-
 	    String REGISTER_SHOPPER="/shoppers";
 
 	    String LOGIN="/users/login";
