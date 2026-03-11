07e0c32f7bcbc70f3e1be8b0a8075507743495d8
R Suthej <R Suthej@FFE-R-Suthej>
2026-03-11 12:15:11 +0530

    Final Commit

--- src/main/java/GenericUtility/ExcelUtility.java
+++ src/main/java/GenericUtility/ExcelUtility.java
@@ -13,7 +13,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ExcelUtility {
 
-	String filepath="C:\\Users\\User\\Downloads\\TestData.xlsx";
+	String filepath="/ShopperStackFrameWork/testdata/TestData.xlsx";
 	
 	public String getDataFromExcel(String sheetName, int rowNum, int colNum) throws EncryptedDocumentException, IOException {
 		FileInputStream fileInputStream=new FileInputStream(filepath);
